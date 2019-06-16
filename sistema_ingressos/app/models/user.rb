d1043c80dd0b5c4a2a5f62ada5b3a49c9694ce04
GabrielBessa <gabrielbessarofl@gmail.com>
2019-06-16 17:34:58 -0300

    Troquei metodo de login pro cpf

--- sistema_ingressos/app/models/user.rb
+++ sistema_ingressos/app/models/user.rb
@@ -4,8 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  validates :email, uniqueness: true
+  validates :cpf, uniqueness: true
   
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
   
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
+
   # User Relations
   has_many :event 
   has_one :ticket
